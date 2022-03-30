aa324b29dea2849038ecc7dd3eeb7c88f3292a64
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2022-03-30 21:43:06 +0200

    feat(Backend): Create model classes

---
+++ Backend/src/main/java/com/example/PKI/model/Certificate.java
@@ -0,0 +1,7 @@
+package com.example.PKI.model;
+
+public class Certificate {
+    private String id;
+    private String file;
+    private boolean isWithdrawn;
+}
---
+++ Backend/src/main/java/com/example/PKI/model/User.java
@@ -0,0 +1,8 @@
+package com.example.PKI.model;
+
+public class User {
+    private String username;
+    private String password;
+    private boolean isCA;
+
+}
