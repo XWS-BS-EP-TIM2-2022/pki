17c35c6505e3ac7d039e58c4ee7506599a057862
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2022-04-01 13:40:20 +0200

    feat(Frontend): Certifications table component

--- Frontend/pki-client/src/app/app-routing.module.ts
+++ Frontend/pki-client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CertificatesTableComponent } from './components/certificates-table/certificates-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'certificates',
+    component: CertificatesTableComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
