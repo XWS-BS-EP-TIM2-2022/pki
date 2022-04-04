928730ee826bb3d161e3b5826626885787c44ea6
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2022-04-04 20:08:06 +0200

    fix(Frontend): Using env for api url

--- Frontend/pki-client/src/app/register/register.service.ts
+++ Frontend/pki-client/src/app/register/register.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,6 @@ export class RegisterService {
   constructor(private _http: HttpClient) { }
 
   registerUser(appUser: any): Observable<any> {
-    return this._http.post<Observable<any>>('http://localhost:8080/users', appUser);
+    return this._http.post<Observable<any>>(environment.api+'users', appUser);
   }
 }
